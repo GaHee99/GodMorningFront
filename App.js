7feb088b69fd93642edc5e9905b1d265a445eb0e
Gahee99 <u1879@sookmyung.ac.kr>
2022-05-13 16:35:35 +0900

    start apiproject

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
+  const [user, setUser] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +23,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
